a12cae95114399f4d6f8ba86945135730e6add77
Настя <nastyr@iMac-Nasta.local>
2018-12-04 16:26:33 +0300

    правки по стилям в Сафаре, фото

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
-const langs = ['az', 'hy', 'be', 'ka', 'kk', 'ky', 'lv', 'lt', 'mo', 'ru', 'tk', 'uk', 'et'];
-const userLang = navigator.language.substring(0, 2) || navigator.userLanguage.substring(0, 2);
+const langs = [
+  "az",
+  "hy",
+  "be",
+  "ka",
+  "kk",
+  "ky",
+  "lv",
+  "lt",
+  "mo",
+  "ru",
+  "tk",
+  "uk",
+  "et"
+];
+const userLang =
+  navigator.language.substring(0, 2) || navigator.userLanguage.substring(0, 2);
 const isRu = l => userLang === l;
 
-if (!localStorage.getItem('lang')) {
-    if (!langs.some(isRu)) { window.location = `${window.location}en.html`; }
-} else if (localStorage.getItem('lang') === 'en') {
-    const currentURL = window.location.pathname.split('.');
-    window.location = `${currentURL[0]}en.html`;
+if (!localStorage.getItem("lang")) {
+  if (!langs.some(isRu)) {
+    window.location = `${window.location}en.html`;
+  }
+} else if (localStorage.getItem("lang") === "en") {
+  const currentURL = window.location.pathname.split(".");
+  window.location = `${currentURL[0]}en.html`;
 }
 
-const setLanguage = (e) => {
-    const langLinks = document.querySelectorAll('[data-lang]');
-    langLinks.forEach((l) => {
-        if (e.target === l) {
-            localStorage.setItem('lang', l.dataset.lang);
-        }
-    });
+const setLanguage = e => {
+  const langLinks = document.querySelectorAll("[data-lang]");
+  langLinks.forEach(l => {
+    if (e.target === l) {
+      localStorage.setItem("lang", l.dataset.lang);
+    }
+  });
 };
 
-const copyEmail = () => {
-  const email = document.querySelector('.copy');
-
-  email.select();
-  document.execCommand('copy');
-};
-
-document.querySelector('.copy').addEventListener('click', copyEmail);
-document.addEventListener('click', setLanguage);
+document.addEventListener("click", setLanguage);
